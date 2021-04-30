4a028a5238a035b8d2802dcd13a082b86a04e7b6
Your Name <you@example.com>
2021-04-30 23:04:33 +0300

    dark-mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { ToggleButton, ToggleButtonGroup, Form } from 'react-bootstrap';
+import { ToggleButton, ToggleButtonGroup, Form, Navbar, Nav } from 'react-bootstrap';
 import './App.css';
 import CardElement from './components/card';
 import './styles/_all.scss';
@@ -14,17 +14,25 @@ function App() {
   }
 
   return (
-    <div>
-      <label id="switch" className="switch">
-        <input type="checkbox" onClick={() => myFunction()} id="slider" />
-        <span className="slider round"></span>
-      </label>
+    <div className='container'>
+
       <h1>Switch me!</h1>
+      <Navbar collapseOnSelect expand="lg" >
+        <label id="switch" className="switch">
+          <input type="checkbox" onClick={() => myFunction()} id="slider" />
+          <span className="slider round"></span>
+        </label>
+        <Nav.Item>
+          <Nav.Link style={{ display: 'inline-block' }} className='text-white mr-3'>Главная</Nav.Link>
+        </Nav.Item>
+      </Navbar>
       <Form>
         <Form.Group>
           <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
         </Form.Group>
       </Form>
+      <Form.Check type="checkbox" label='CheckBox' />
+      <Form.Check type="radio" label='Radio' />
       <ToggleButtonGroup className='d-flex justify-content-center align-items-center' type="radio" name="options" defaultValue={0}>
         {[<ViewComfyIcon />, <FormatListBulletedIcon />].map((item, index) =>
           <ToggleButton key={index} value={index} variant='light' checked={0 === index}>{item}</ToggleButton>
